511960020b31bad3ebcf0e373dc3b7e018ea5d43
fikrisabiq <81612058+fikrisabiq@users.noreply.github.com>
2026-09-18 19:41:13 +0700

    fourth commit

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -9,13 +9,4 @@ export const validate = (schema) => (req, res, next) => {
   req.validated = value;
   next();
 };
-
-export const validateQuery = (schema) => (req, res, next) => {  const { error, value } = schema.validate(req.query, {
-  abortEarly: false,
-  allowUnknown: false,
-  stripUnknown: true
-});
-if (error) return next(error);
-req.validated = value;
-next();
-};
+export default validate;
--- src/services/users/validator/schema.js
+++ src/services/users/validator/schema.js
@@ -1,8 +1,10 @@
 import Joi from 'joi';
 
-export const userPayloadSchema = Joi.object({
+const userPayloadSchema = Joi.object({
   name: Joi.string().min(3).max(255),
   email: Joi.string().email().required(),
   role: Joi.string().valid('user', 'admin').required(),
   password: Joi.string().min(8).required(),
 });
+
+export default userPayloadSchema;
